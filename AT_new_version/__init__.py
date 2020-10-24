ef23a541a96e1accc01fa5ee51012c896c2a73da
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2020-10-24 23:00:50 +0100

    Update __init__.py

--- AT_new_version/__init__.py
+++ AT_new_version/__init__.py
@@ -1,6 +1,5 @@
-from .Indicators import  ( mms, mme, macd, rsi, bollinger, momentum, 
-							rate_of_change, stochastique,obv, williams,
-							MFI, cho, nvi, pvi)
+from .Indicators import  ( mms, mme, macd, rsi, bollinger, momentum, rate_of_change,
+			  stochastique,obv, williams,MFI, cho, nvi, pvi)
 from .Signal import (signal_mms1,signal_mms2,signal_macd1,signal_macd2,signal_rsi,signal_bollinger,
 					signal_momentum, signal_stochastique1,signal_stochastique2,signal_obv,
 					signal_roc,signal_mfi,signal_cho,signal_nvi,signal_pvi) 
@@ -10,4 +9,4 @@ from .GraphIndicators import (plot_mms,plot_mme,plot_macd,plot_rsi,plot_bollinge
 from .GraphSignal import (plt_mms1,plt_mms2,plt_macd1,plt_macd2,plt_rsi,plt_bollinger,plt_momentum,
 							plt_stochastique1,plt_stochastique2,plt_obv,plt_roc,plt_mfi,plt_cho,
 							plt_nvi,plt_pvi )
-from .Performance import (pmv,Dietz)
+from .Performance import pmv,Dietz
