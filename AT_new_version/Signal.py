c98711f46f1b53eacb7141bfeccb12a3481ec3ac
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2020-10-31 18:34:01 +0100

    Update Signal.py

--- AT_new_version/Signal.py
+++ AT_new_version/Signal.py
@@ -4,37 +4,124 @@ import matplotlib.pyplot as plt
 from .Indicators import *
 from .utils import adjustsignal 
 
-def signal_mms1(df,n):
+def signal_smm(df,n):
+	"""
+	Trading signals of rule1 of simple moving average 
+	For more information about rule1 see documentation 
+		Inputs: 
+				Input | Type                             | Description
+				=================================================================================
+				 df   |pandas.DataFrame or pandas.Series | Prices, volumes .....
+				 n    | int                              | Period
+		Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	    **Signals:   1----> Buy
+	                 0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal['signal']=0
 	signal["compa"]=0
-	MMS=mms(df,n)["MMS"]
+	MMS=smm(df,n)["SMM"]
 	signal["compa"].loc[n:]=np.where(df[n:] > MMS[n:],1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
-def signal_mms2(df,ws,wl):
+def signal_smm2(df,ws,wl):
+	"""
+	Trading signals of rule2 of simple moving average 
+	For more information about rule2 see documentation 	 
+	Inputs: 
+			Input   | Type                             | Description
+			=================================================================================
+			 df     |pandas.DataFrame or pandas.Series | Prices, volumes .....
+	         ws     |int                               | The period of the shorter moving average
+	         wl     |int                               | The periode if the longer moving average
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal['compa']=np.nan
-	signal["MMS_COURT"]=mms(df,ws)["MMS"]
-	signal["MMS_LONG"]=mms(df,wl)["MMS"]
+	signal["MMS_COURT"]=smm(df,ws)["SMM"]
+	signal["MMS_LONG"]=smm(df,wl)["SMM"]
 	signal["compa"].loc[wl:]=np.where(signal["MMS_COURT"][wl:] > signal["MMS_LONG"][wl:],1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_macd1(df,ws,wl):
+	"""
+	Trading signals using rule1 of Moving Average Convergence Divegence
+	For more information about rule1 see documentation 
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         ws    |int                               | The period of the shorter moving average
+	         wl    |int                               | The periode if the longer moving average
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
+
 	signal=pd.DataFrame(index=df.index)
 	MACD=macd(df,ws,wl)["MACD"]
 	signal["compa"]=np.nan
 	signal["compa"][wl:]=np.where(MACD[wl:]>0 ,1 ,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_macd2(df,ws,wl,wsig=9):
+	"""
+	Trading signals using rule2 the Moving Average Convergence Divegence
+	For more information about rule2 see documentation 
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         ws    |int                               | The period of the shorter moving average
+	         wl    |int                               | The periode if the longer moving average
+	         wsig* |int                               | The period of the signal line 
+	    * By default wsig= 9
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan
 	MACD=macd(df,ws,wl,wsig)[["MACD","MACDsignal"]]
 	signal["compa"][wl:]=np.where(MACD["MACD"][wl:] > MACD["MACDsignal"][wl:] ,1 ,0 )
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def  signal_rsi(df,n):
+	"""
+	Trading signals using  Relative Strength index (RSI)
+	Inputs: 
+	        Input | Type                             | Description
+	       =========================================================================================
+	         df   |pandas.DataFrame or pandas.Series | Prices, volumes .....
+	         n    | int                              | Period
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	rs=rsi(df,n)["RSI"]
 	signal["compa_achat"]=np.nan
@@ -47,7 +134,24 @@ def  signal_rsi(df,n):
 	sig=adjustsignal(sig)
 	sig=pd.Series(sig, index=df.index)
 	return sig
+
+
 def signal_bollinger(df,w,k):
+	"""
+	trading signals using  the Bollinger Bands
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         w     |int                               | Periods of moving average
+	         k     |int                               | The number of stradard diviations
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan
 	signal["compa2"]=np.nan
@@ -60,16 +164,54 @@ def signal_bollinger(df,w,k):
 	sig=adjustsignal(sig)
 	sig=pd.Series(sig, index=df.index)
 	return sig
+
+
 def signal_momentum(df,w,wsig=9):
+	"""
+	Trading signals using the momentum
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         w     |int                               | The period 
+	         wsig* |int                               | The period of the signal line 
+	    * By default wsig= 9
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	MOM=momentum(df,w,wsig=9)[["MOM","MOMsignal"]]
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan
 	signal["compa"][w:]=np.where(MOM["MOM"][w:] > MOM["MOMsignal"][w:],1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
-def signal_stochastique1(df,high,low ,n , w):
+
+
+def signal_stochastic1(df,high,low ,n , w):
+	"""
+	Trading signals using rule1 the Stochastic oscillator : %K and %D
+	For more information about rule1 see documentation 
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         high  |int                               | Highest high 
+	         low   |int                               | Lowest low
+	         n     |int                               | %K periods
+	         w     |int                               | %D periods 
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
-	k=stochastique(df ,high,low, n , w)["%K"]
+	k=stochastic(df ,high,low, n , w)["%K"]
 	signal["compa_achat"]=np.nan
 	signal["compa_vente"]=np.nan
 	signal["compa_achat"][n:]=np.where(k[n:] > 20,1,0)
@@ -80,14 +222,50 @@ def signal_stochastique1(df,high,low ,n , w):
 	sig=adjustsignal(sig)
 	sig=pd.Series(sig, index=df.index)
 	return sig
-def signal_stochastique2(df,high,low ,n , w):
+
+
+def signal_stochastic2(df,high,low ,n , w):
+	"""
+	Trading signals using rule2 the Stochastic oscillator : %K and %D
+	For more information about rule2 see documentation 
+	Inputs: 
+	        Input  | Type                             | Description
+	       =========================================================================================
+	         df    |pandas.DataFrame                  | Prices, volumes .....
+	         high  |int                               | Highest high 
+	         low   |int                               | Lowest low
+	         n     |int                               | %K periods
+	         w     |int                               | %D periods 
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
-	k=stochastique(df ,high,low, n , w)[["%K","%D"]]
+	k=stochastic(df ,high,low, n , w)[["%K","%D"]]
 	signal["compa"]=np.nan
 	signal["compa"][n+w :]=np.where(k["%K"][n+w:] > k["%D"][n+w:],1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_obv(df,vol,n):
+	"""
+	Trading signals of  the On Balance Volume (OBV) & the signal line of OBV
+	Inputs: 
+	        Input | Type                             | Description
+	       =========================================================================================
+	         df   |pandas.DataFrame                  | Prices
+	         vol  |pandas.DataFrame                  | Volumes
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	ob=obv(df,vol)["OBV"]
 	obs=pd.Series(ob.rolling(n).mean(), name='obvsignal')
@@ -95,14 +273,52 @@ def signal_obv(df,vol,n):
 	signal["compa"][n:]=np.where( ob[n:]  > obs[n:],1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_roc(df,w):
+	"""
+	Trading signals using the Rate of change (ROC)
+	Inputs: 
+	        Input | Type                             | Description
+	       =========================================================================================
+	         df   |pandas.DataFrame or pandas.Series | Prices, volumes .....
+	         w    | int                              | Period
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	roc=rate_of_change(df,w)["ROC"]
 	signal["compa"]=np.nan
 	signal["compa"][w:]=np.where( roc[w:]> 0,1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return  signal["signal"]
+
+
 def signal_mfi(df,vol,high,low,n):
+	"""
+	Trading signals using  the Money Flow Index (MFI)
+	Inputs: 
+	        Input   | Type                             | Description
+	       =========================================================================================
+	         close  |pandas.DataFrame or pandas.Series | Prices
+	         volume |pandas.DataFrame or pandas.Series | Volumes
+	         High   |pandas.DataFrame or pandas.Series | Highest high 
+	         low    |pandas.DataFrame or pandas.Series | Lowest low
+	         n      |int                               | Periods 
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+
+	"""
+
+
 	signal=pd.DataFrame(index=df.index)
 	mfi=MFI(df,vol,high,low,n)['MFI']
 	signal["compa_achat"]=np.nan
@@ -115,14 +331,51 @@ def signal_mfi(df,vol,high,low,n):
 	sig=adjustsignal(sig)
 	sig=pd.Series(sig, index=df.index)
 	return sig
+
+
 def signal_cho(df,vol,high,low,n,ws,wl):
+	"""
+	Plots trading signals using the Chaikin Oscillator
+	Inputs: 
+	        Input   | Type                             | Description
+	       =========================================================================================
+	         close  |pandas.DataFrame or pandas.Series | Prices
+	         volume |pandas.DataFrame or pandas.Series | Volumes
+	         High   |pandas.DataFrame or pandas.Series | Highest high 
+	         low    |pandas.DataFrame or pandas.Series | Lowest low
+	         n      |int                               | Periods
+	         ws     |int                               | The period of the shorter moving average
+	         wl     |int                               | The periode if the longer moving average
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan 
 	ch=cho(df,vol,high,low,n,ws,wl)["CHO"]
 	signal["compa"][ws+wl:]=np.where(ch[ws+wl:] > 0, 1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_nvi(df,vol,n):
+	"""
+	Trading signals using the Negative Volume Index (NVI) & the signal line 
+	Inputs: 
+	        Input    | Type                             | Description
+	       =========================================================================================
+	         close   |pandas.DataFrame                  | Prices
+	         volume  |pandas.DataFrame                  | Volumes
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan 
 	nv=nvi(df,vol)
@@ -130,7 +383,23 @@ def signal_nvi(df,vol,n):
 	signal["compa"][n:]=np.where(nv[n:] > nvis[n:] ,1,0)
 	signal["signal"]=adjustsignal(signal["compa"].diff())
 	return signal["signal"]
+
+
 def signal_pvi(df,vol,n):
+	"""
+	Trading signals using the Positive Volume Index (PVI) & the signal line 
+	Inputs: 
+	        Input    | Type                             | Description
+	       =========================================================================================
+	         close   |pandas.DataFrame                  | Prices
+	         volume  |pandas.DataFrame                  | Volumes
+	Outputs:
+				Output | Type                             | Description
+				================================================================================= 
+				       | pandas.Series                    | Signals**
+	**Signals:   1----> Buy
+	             0----> Sell
+	"""
 	signal=pd.DataFrame(index=df.index)
 	signal["compa"]=np.nan 
 	pv=pvi(df,vol)
